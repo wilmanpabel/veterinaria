d8ded328837e8be01ad2fb42190decd57e306dc0
Wilman Pabel <wilmanpabel@gmail.com>
2018-12-02 18:50:04 -0500

    starter commit

---
+++ src/main/java/com/pabel/veterinaria/VeterinariaApplication.java
@@ -0,0 +1,12 @@
+package com.pabel.veterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VeterinariaApplication.class, args);
+    }
+}
