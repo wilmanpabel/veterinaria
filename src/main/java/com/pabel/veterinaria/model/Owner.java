d326b40a3d43db808589089a6169daaddb9a67a0
Wilman Pabel <wilmanpabel@gmail.com>
2018-12-09 16:42:52 -0500

    se agrego los Pojos Closes #9.

---
+++ src/main/java/com/pabel/veterinaria/model/Owner.java
@@ -0,0 +1,4 @@
+package src.main.java.com.pabel.veterinaria.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/pabel/veterinaria/model/PetType.java
@@ -0,0 +1,13 @@
+package src.main.java.com.pabel.veterinaria.model;
+
+public class PetType {
+    private String  name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
